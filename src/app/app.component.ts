9bf3bb2ffbf3013e28004997bd8973412aea70b4
kareem mohamed <kareem_mohamed94@outlook.com>
2024-09-29 16:54:55 +0300

    Creating MemberComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { MemberComponent } from './member/member.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MemberComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
