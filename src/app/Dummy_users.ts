653a755813c75266bd5009f3f14f489b99dfe971
kareem mohamed <kareem_mohamed94@outlook.com>
2024-09-29 17:30:48 +0300

    Adding styling to MemberComponent + Adding members images

---
+++ src/app/Dummy_users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 1,
+    name: 'Jasmine Washington',
+    avatar: '1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Emily Thompson',
+    avatar: '2.jpg',
+  },
+  {
+    id: 3,
+    name: 'Marcus Johnson',
+    avatar: '3.jpg',
+  },
+  {
+    id: 4,
+    name: 'David Miller',
+    avatar: '4.jpg',
+  },
+  {
+    id: 5,
+    name: 'Priya Patel',
+    avatar: '5.jpg',
+  },
+  {
+    id: 6,
+    name: 'Arjun Singh',
+    avatar: '6.jpg',
+  },
+];
