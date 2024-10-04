af4bcf8d0c0c952df7dae875f1efe6add0359ad9
kareem mohamed <kareem_mohamed94@outlook.com>
2024-10-04 20:54:02 +0300

    Adding task.component.html markup

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_TASKS } from '../Dummy_tasks';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
