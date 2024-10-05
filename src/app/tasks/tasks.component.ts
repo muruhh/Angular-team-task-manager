c292ae18f9ce6a64172ef9a9fb2f283f4170e8c2
kareem mohamed <kareem_mohamed94@outlook.com>
2024-10-05 18:38:10 +0300

    Adding open logic to new task form

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ɵDeferBlockConfig } from '@angular/core';
 import { DUMMY_TASKS } from '../Dummy_tasks';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from '../new-task/new-task.component';
 
 interface User {
   id: number;
@@ -11,16 +12,21 @@ interface User {
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) selectedMember!: User;
+  openForm: boolean = false;
 
   tasks = DUMMY_TASKS;
 
   get selectedTasks() {
     return this.tasks.filter((task) => task.userId === this.selectedMember.id);
+  }
+
+  openNewTaskForm() {
+    this.openForm = true;
   }
 }
